c184d6886cd6b3b063ed02892d3271819692639b
lz20r <zhiyaonaiara@gmail.com>
2024-09-24 13:46:03 +0200

    Refactor array operations and sort alphabetically

--- asignaturas/DWES/Martes 24 septiembre/ejercicios/ej12.php
+++ asignaturas/DWES/Martes 24 septiembre/ejercicios/ej12.php
@@ -35,30 +35,38 @@
 
     <?php
     echo "<p> 1º) Crear una matriz con 4 elementos. </p>";
-    $array = array("elemento1", "elemento2", "elemento3", "elemento4");
+    $array = array("París", "Londres", "Roma", "Madrid"); 
     print_r($array) . "<br>"; 
 
     echo "<p> 2º) Muestra el segundo elemento de la matriz. </p>";
     echo "El segundo elemento es: " . $array[1] . "<br>";
  
     echo "<p> 3º) Añade al final otro elemento y muéstralo. </p>";
-    $array[] = "elemento5";
+    $array[] = "Berlín";
     echo "El último elemento es: " . $array[4] . "<br>";
 
     echo "<p> 4º) Cambiar el valor del primer elemento de la matriz. </p>";
-    echo "El primer elemento antes de cambiar es: " . $array[0] . "<br>";
-    $array[0] = "elemento0";
+    echo "El primer elemento antes de cambiar es: " . $array[0] . "<br>"; 
+    $array[0] = "Tokio";
     echo "El primer elemento después de cambiar es: " . $array[0] . "<br>";
+
 
     echo "<p> 5º) Muestra cuantos elementos contiene el array. </p>";
     echo "El array contiene " . count($array) . " elementos. <br>";
 
 
     echo "<p> 6º) Recorre el array con un foreach o un for y muestra todos los elementos ordenados alfabéticamente. </p>";
-    echo "La matriz es:" . "<br>"; 
-    foreach ($array as $elemento) {
-        echo $elemento . "<br>";
+    echo "La matriz original es: ";
+    for ($i = 0; $i < count($array); $i++) {
+        echo $array[$i] . ", ";
     }
+    echo "<br>";
+    
+    echo "La matriz ordenada alfabéticamente es: ";
+    sort($array);
+    foreach ($array as $elemento) {
+        echo $elemento .  ", ";
+    } 
     echo "<br>";
     ?>
 </body>
